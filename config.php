983d8659b3b8f4eecc8602233ec3b334c3caa75b
Muhammad Irfanudin <barospkl1234@gmail.com>
2025-11-21 22:18:25 +0700

    Update database connection settings in config.php

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
 // config.php
-$host = "db.fr-pari1.bengt.wasmernet.com";
-$port = "10272"; 
-$username = "e9d7f9f572e480006d6d45eb88a6"; 
-$password = "0691e9d7-f9f5-74a7-8000-1f06d3903053";
-$database = "user_app";
+$host = "localhost";
+$username = "root"; 
+$password = "";
+$database = "user";
 
 // Membuat koneksi
-$conn = new mysqli($host,$port, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database);
 
 // Cek koneksi
 if ($conn->connect_error) {
@@ -17,4 +16,3 @@ if ($conn->connect_error) {
 // Set charset
 $conn->set_charset("utf8mb4");
 ?>
-
